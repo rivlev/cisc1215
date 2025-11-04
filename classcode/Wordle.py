4d30cc38f6993599768de14b3ff37922daae92ed
Justin <justinyijiachen@gmail.com>
2025-11-04 14:06:45 -0500

    updated coloring logic (error with yellow letters, i.e. printing letter yellow twice when it only occur once in secret_word) to color-code correctly, updated print message logic when game is completed

--- classcode/Wordle.py
+++ classcode/Wordle.py
@@ -31,6 +31,7 @@ def count_letters(word):
 def wordle():
     legal_words = loadDictionary()
     secret_word = choice(list(legal_words))
+    secret_word = "locks"
     secret_word_letter_counts = count_letters(secret_word)
 
     for g in range(NUM_GUESSES):
@@ -40,25 +41,34 @@ def wordle():
 
         if guess == secret_word:
             print(GREEN_PREFIX + guess + COLOR_POSTFIX)
-            print("You guessed it!")
-            break
+            print(f"You guessed it in {g+1} tr{"y" if g == 0 else "ies"}!")
+            return
 
         # guess is valid
         # now score it
-        result = [''] * NUM_LETTERS
+        # initially, all letters are by default gray
+        result = [GRAY_PREFIX + c + COLOR_POSTFIX for c in list(guess)]
         guess_letter_counts = secret_word_letter_counts.copy()
         # find green letters
         for i, c in enumerate(guess):
-            if guess[i] == secret_word[i]:
+            # if a letter is not in secret word: it stays gray, check next letter
+            if guess[i] not in secret_word:
+                continue
+
+            # else the letter must be in secret word
+            # if it's at the correct location, it's green, decrement frequency dict
+            elif guess[i] == secret_word[i]:
                 result[i] = GREEN_PREFIX + guess[i] + COLOR_POSTFIX
                 guess_letter_counts[c] -= 1
-        for i, c in enumerate(guess):
-            if result[i] != '':
-                continue
-            if c not in guess_letter_counts or guess_letter_counts[c] == 0:
-                result[i] = GRAY_PREFIX + guess[i] + COLOR_POSTFIX
-            else:
+
+            # else the letter is in secret word, but not at the right location
+            # if frequency dict still have letters, color letter yellow and decrement count
+            elif guess_letter_counts[c] > 0:
                 result[i] = YELLOW_PREFIX + guess[i] + COLOR_POSTFIX
+                guess_letter_counts[c] -= 1
+            
+            # else: letter is in secret word, incorrect location, no more count in freq dict
+            # color would be gray, no change from default color gray
 
         # print result
         print(''.join(result) + "\n")
